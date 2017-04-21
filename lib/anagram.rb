c98539497982d11db9612df5d11dc43769c09d4a
ericschoettle <eric.schoettle@gmail.com>
2017-04-21 13:44:58 -0700

    meet spec 'remove non-word characters'

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,12 @@ require "pry"
 class String
   define_method(:anagram_handler) do |word2|
     word1 = self
-    word1_lower = word1.downcase
-    word2_lower = word2.downcase
+    word1_prep, word2_prep = word1.prep(word2)
     result = "'" + word1.capitalize() + "' and '" + word2 + "' are"
-    if word1_lower.anagram?(word2_lower)
+    if word1_prep.anagram?(word2_prep)
       result = result + " anagrams and "
       result = self.palindrome_handler(word2, result)
-    elsif word1_lower.antigram?(word2_lower)
+    elsif word1_prep.antigram?(word2_prep)
       result = result + " antigrams and "
       result = self.palindrome_handler(word2, result)
     else
@@ -23,15 +22,14 @@ end
 class String
   define_method(:palindrome_handler) do |word2, result|
     word1 = self
-    word1_lower = word1.downcase
-    word2_lower = word2.downcase
-    if word1_lower.palindrome? && word2_lower.palindrome?
+    word1_prep, word2_prep = word1.prep(word2)
+    if word1_prep.palindrome? && word2_prep.palindrome?
       result.concat("both are palindromes")
-    elsif (word1_lower + word2_lower).palindrome?
+    elsif (word1_prep + word2_prep).palindrome?
       result.concat("are palindromes of each other")
-    elsif word1_lower.palindrome?
+    elsif word1_prep.palindrome?
       result.concat("'" + word1 + "' is a palindrome")
-    elsif word2_lower.palindrome?
+    elsif word2_prep.palindrome?
       result.concat("'" + word2 + "' is a palindrome")
     else
       result = result [0...-5]
@@ -42,20 +40,26 @@ end
 
 class String
   define_method(:antigram?) do |word2|
-    word1 = self
-    /[#{Regexp.quote(word1)}]/ !~ word2
+    /[#{Regexp.quote(self)}]/ !~ word2
   end
 end
 
 class String
   define_method(:anagram?) do |word2|
-    word1 = self
-    word1.chars.sort == word2.chars.sort
+    self.chars.sort == word2.chars.sort
   end
 end
 
 class String
   define_method(:palindrome?) do
     self == self.reverse
+  end
+end
+
+class String
+  define_method(:prep) do |word2|
+    word1_prep = self.downcase.tr('^a-z', '')
+    word2_prep = word2.downcase.tr('^a-z', '')
+    return word1_prep, word2_prep
   end
 end

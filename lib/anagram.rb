4ac53a15510c6a8f7f2840099b9f90aaeaf6bcaf
ericschoettle <eric.schoettle@gmail.com>
2017-04-21 10:51:45 -0700

    refactor to use palindrome handler and update specs accordingly

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,7 @@ class String
     result = "'" + word1.capitalize() + "' and '" + word2 + "' are"
     if word1_lower.anagram?(word2_lower)
       result = result + " anagrams and "
-      if word1_lower.palindrome? && word2_lower.palindrome?
-        result.concat("both are palindromes")
-      elsif word1_lower.palindrome?
-        result.concat("'" + word1 + "' is a palindrome")
-      elsif word2_lower.palindrome?
-        result.concat("'" + word2 + "' is a palindrome")
-      else
-        result = result.chomp(" and ")
-      end
+      result = self.palindrome_handler(word2, result)
     end
     result
   end
@@ -29,21 +21,23 @@ class String
   end
 end
 
-# class String
-#   define_method(:palindrome_handler) do |word2|
-#     word1_lower = self.downcase
-#     word2_lower = word2.downcase
-#     result = ""
-#     if word1_lower.anagram(word2_lower)
-#       result = word1 + " and " + word2 "are anagrams"
-#       if word1_lower.palindrome
-#         result.concat("and" + word1 + "is a palindrome")
-#       elsif word2_lower.palindrome
-#         result.concat("and" + word2 + "is a palindrome")
-#       end
-#     end
-#   end
-# end
+class String
+  define_method(:palindrome_handler) do |word2, result|
+    word1 = self
+    word1_lower = word1.downcase
+    word2_lower = word2.downcase
+    if word1_lower.palindrome? && word2_lower.palindrome?
+      result.concat("both are palindromes")
+    elsif word1_lower.palindrome?
+      result.concat("'" + word1 + "' is a palindrome")
+    elsif word2_lower.palindrome?
+      result.concat("'" + word2 + "' is a palindrome")
+    else
+      result = result [0...-5]
+    end
+    result
+  end
+end
 
 class String
   define_method(:palindrome?) do
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -14,22 +14,19 @@ describe('String.anagram') do
   it("tests if words are both anagrams of each other and if both are palindromes") do
     expect("otto".anagram_handler("toot")).to(eq("'Otto' and 'toot' are anagrams and both are palindromes"))
   end
-  it("tests if words are not anagrams and if either is a palindrome") do
-    expect("mom".anagram_handler("word")).to(eq("'Mom' and 'word' aren't anagrams but 'mom' is a palindrome"))
-  end
-  it("tests if words are not anagrams and if both are palindromes") do
-    expect("mom".anagram_handler("word")).to(eq("'Mom' and 'dad' aren't anagrams but both are palindromes"))
-  end
   it("tests if words are palindromes of each other") do
     expect("Meg".anagram_handler("gem")).to(eq("'Meg' and 'gem' are anagrams and palindromes of each other"))
-  end
-  it("tests if words are indeed words") do
-    expect("ggmbph".anagram_handler("mbpghg")).to(eq("enter words, you fool"))
   end
   it("tests if words are antigrams") do
     expect("art".anagram_handler("mall")).to(eq("'Art' and 'mall' are antigrams"))
   end
+  it("tests if words are not anagrams or antigrams") do
+    expect("mom".anagram_handler("word")).to(eq("'Eve' and 'ewe' aren't anagrams or antigrams but both are palindromes"))
+  end
   it("can handle multi-word input") do
     expect("art mall".anagram_handler("genders")).to(eq("'Art mall' and 'genders' are antigrams"))
+  end
+  it("tests if words are indeed words") do
+    expect("ggmbph".anagram_handler("mbpghg")).to(eq("enter words, you fool"))
   end
 end

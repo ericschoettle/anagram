635f1660b1e7d2fbe5707d6723d3315796d4fca1
ericschoettle <eric.schoettle@gmail.com>
2017-04-21 09:18:18 -0700

    pass case-insensitive spec

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,14 @@
 require "pry"
 
 class String
+  define_method(:anagram_handler) do |word2|
+    word1 = self.downcase
+    word2 = word2.downcase
+    word1.anagram(word2)
+  end
+end
+
+class String
   define_method(:anagram) do |word2|
     word1 = self
     word1.chars.sort == word2.chars.sort
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,24 +3,24 @@ require('./lib/anagram')
 
 describe('String.anagram') do
   it("tests if a word is an anagram") do
-    expect("star".anagram("rats")).to(eq(true))
+    expect("star".anagram_handler("rats")).to(eq(true))
   end
   it("is case insensitive") do
-    expect("Meg".anagram("gem")).to(eq(true))
+    expect("Meg".anagram_handler("gem")).to(eq(true))
   end
   it("tests if words are palindromes of themselves") do
-    expect("mom".anagram("dad")).to(eq("these words aren't anagrams, but 'mom' and 'dad' are both palindromes!"))
+    expect("mom".anagram_handler("dad")).to(eq("these words aren't anagrams, but 'mom' and 'dad' are both palindromes!"))
   end
   it("tests if words are palindromes of each other") do
-    expect("Meg".anagram("gem")).to(eq("these words are palindromes of each other"))
+    expect("Meg".anagram_handler("gem")).to(eq("these words are palindromes of each other"))
   end
   it("tests if words are indeed words") do
-    expect("ggmbph".anagram("mbpghg")).to(eq("enter words, you fool!"))
+    expect("ggmbph".anagram_handler("mbpghg")).to(eq("enter words, you fool!"))
   end
   it("tests if words are antigrams") do
-    expect("art".anagram("mall")).to(eq("'art' and 'mall' are antigrams"))
+    expect("art".anagram_handler("mall")).to(eq("'art' and 'mall' are antigrams"))
   end
   it("can handle multi-word input") do
-    expect("art mall".anagram("genders")).to(eq("'art mall' and 'genders' are antigrams"))
+    expect("art mall".anagram_handler("genders")).to(eq("'art mall' and 'genders' are antigrams"))
   end
 end

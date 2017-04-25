55eb26f014fd42ade5ab9c22d68a508a8fa68771
ericschoettle <eric.schoettle@gmail.com>
2017-04-24 17:53:35 -0700

    refactored to use instance variables

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,23 +3,24 @@ require "rest-client"
 
 class String
   define_method(:anagram_handler) do |word2|
-    word1 = self
-    word1_prep, word2_prep = word1.prep(word2)
-    result = "'" + word1.capitalize() + "' and '" + word2 + "' are"
-    if word1 != word2
-      if word1.test_word(word2) == true
-        if word1_prep.anagram?(word2_prep)
+    @word1 = self
+    @word2 = word2
+    @word1_prep, @word2_prep = @word1.prep(@word2)
+    result = "'" + @word1.capitalize() + "' and '" + @word2 + "' are"
+    if @word1 != @word2
+      if self.test_word() == true
+        if self.anagram?()
           result = result + " anagrams and "
-          result = self.palindrome_handler(word2, result)
-        elsif word1_prep.antigram?(word2_prep)
+          result = self.palindrome_handler(result)
+        elsif self.antigram?()
           result = result + " antigrams and "
-          result = self.palindrome_handler(word2, result)
+          result = self.palindrome_handler(result)
         else
           result = result + " neither anagrams nor antigrams but "
-          result = self.palindrome_handler(word2, result)
+          result = self.palindrome_handler(result)
         end
       else
-        result = word1.test_word(word2).capitalize + " isn't a word!"
+        result = self.test_word().capitalize + " isn't a word!"
       end
     else
       result = "Trying to be clever, putting in the same word twice..."
@@ -27,12 +28,10 @@ class String
 
     result
   end
-end
 
-class String
-  define_method(:test_word) do |word2|
+  define_method(:test_word) do
     test_result = true
-    wordsArray = self.split(" ") + word2.split(" ")
+    wordsArray = @word1.split(" ") + @word2.split(" ")
     wordsArray.each() do |word|
       word = word.prep("").join("")
       url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=dc368593-f759-4a0f-b8da-2e87f78e0fed"
@@ -42,46 +41,34 @@ class String
     end
     test_result
   end
-end
 
-class String
-  define_method(:palindrome_handler) do |word2, result|
-    word1 = self
-    word1_prep, word2_prep = word1.prep(word2)
-    if word1_prep.palindrome? && word2_prep.palindrome?
+  define_method(:palindrome_handler) do |result|
+    if @word1_prep.palindrome? && @word2_prep.palindrome?
       result.concat("both are palindromes")
-    elsif (word1_prep + word2_prep).palindrome?
+    elsif (@word1_prep + @word2_prep).palindrome?
       result.concat("are palindromes of each other")
-    elsif word1_prep.palindrome?
-      result.concat("'" + word1 + "' is a palindrome")
-    elsif word2_prep.palindrome?
-      result.concat("'" + word2 + "' is a palindrome")
+    elsif @word1_prep.palindrome?
+      result.concat("'" + @word1 + "' is a palindrome")
+    elsif @word2_prep.palindrome?
+      result.concat("'" + @word2 + "' is a palindrome")
     else
       result = result [0...-5]
     end
     result
   end
-end
 
-class String
-  define_method(:antigram?) do |word2|
-    /[#{Regexp.quote(self)}]/ !~ word2
+  define_method(:antigram?) do
+    /[#{Regexp.quote(@word1)}]/ !~ @word2
   end
-end
 
-class String
-  define_method(:anagram?) do |word2|
-    self.chars.sort == word2.chars.sort
+  define_method(:anagram?) do
+    @word1_prep.chars.sort == @word2_prep.chars.sort
   end
-end
 
-class String
   define_method(:palindrome?) do
     self == self.reverse
   end
-end
 
-class String
   define_method(:prep) do |word2|
     word1_prep = self.downcase.tr('^a-z', '')
     word2_prep = word2.downcase.tr('^a-z', '')
